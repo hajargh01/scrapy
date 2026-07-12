f79ba7294a3f0f2a223832544e4a276c6aee64d6
hajar <ghounaimihajar@gmail.com>
2026-07-12 17:16:11 +0100

    scrape some data; add spec

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import requests
 from payload import data
+from bs4 import BeautifulSoup
 
-response = requests.post(
-    "https://www.marchespublics.gov.ma/index.php?page=entreprise.EntrepriseAdvancedSearch&searchAnnCons",
-    headers={
-        "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/125.0.0.0 Safari/537.36"
-        ),
-    },
-    data=data,
-)
+# response = requests.post(
+#     "https://www.marchespublics.gov.ma/index.php?page=entreprise.EntrepriseAdvancedSearch&searchAnnCons",
+#     headers={
+#         "Content-Type": "application/x-www-form-urlencoded",
+#         "User-Agent": (
+#             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+#             "AppleWebKit/537.36 (KHTML, like Gecko) "
+#             "Chrome/125.0.0.0 Safari/537.36"
+#         ),
+#     },
+#     data=data,
+# )
+# text = response.text
+# with open("index.html", "w", encoding="utf-8") as f:
+#     f.write(text)
+with open("index.html", encoding="utf-8") as f:
+    text = f.read()
 
+soup = BeautifulSoup(text, "html.parser")
+dates = soup.select("[headers=cons_ref] div:nth-child(4)")
+l = []
+for date in dates:
+    l.append(date.text.strip())
+references = soup.select("[headers=cons_intitule] .ref")
+R = []
+for reference in references:
+    R.append(reference.text)
+objects = soup.select("[headers=cons_intitule] div:nth-child(2) div")
+O = []
+for _object in objects:
+    O.append(_object.text)
 
-text = response.text
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(text)
---
+++ spec.md
@@ -0,0 +1,8 @@
+## Informations à récupérer
+
+- Publié le
+- Référence
+- Objet
+- Acheteur public
+- Lieu d'exécution
+- Date limite de remise des plis
